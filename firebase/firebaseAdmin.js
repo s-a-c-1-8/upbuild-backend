cb2d8d2bbe12620a067f5c81494c9c68958e04fd
s-a-c-1-8 <sachindenki98@gmail.com>
2025-12-23 17:58:00 +0530

    error-fixed

--- firebase/firebaseAdmin.js
+++ firebase/firebaseAdmin.js
@@ -1,10 +1,10 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-service.json");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
 }
-
 module.exports = admin;
